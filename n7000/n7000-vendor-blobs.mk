700a0b99517479b7352415211e4a1ff4d37d25ac
Hiemanshu Sharma <hiemanshu@gmail.com>
2012-08-27 23:26:51 -0500

    Add galaxys2-common and missing n7000 files
    Change-Id: I409fdc625a30b3c0333dad8c9203aad811a6ad44

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -178,3 +178,30 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/audio/usr/share/alsa/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
     $(LOCAL_PATH)/proprietary/audio/usr/share/alsa/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
     $(LOCAL_PATH)/proprietary/audio/usr/share/alsa/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf
+
+# Prebuilt libraries that are needed to build open-source libraries
+PRODUCT_COPY_FILES := \
+    vendor/samsung/n7000/proprietary/system/lib/libril.so:obj/lib/libril.so \
+    vendor/samsung/n7000/proprietary/system/lib/libsecril-client.so:obj/lib/libsecril-client.so
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/n7000/proprietary/system/bin/rild:system/bin/rild \
+    vendor/samsung/n7000/proprietary/system/bin/gps.cer:system/bin/gps.cer \
+    vendor/samsung/n7000/proprietary/system/bin/gpsd:system/bin/gpsd \
+    vendor/samsung/n7000/proprietary/system/bin/bcm4330B1.hcd:system/bin/bcm4330B1.hcd \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/bcm4330_apsta.bin:system/etc/wifi/bcm4330_apsta.bin \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/bcm4330_mfg.bin:system/etc/wifi/bcm4330_mfg.bin \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/bcm4330_p2p.bin:system/etc/wifi/bcm4330_p2p.bin \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/bcm4330_sta.bin:system/etc/wifi/bcm4330_sta.bin \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/nvram_mfg.txt_murata:system/etc/wifi/nvram_mfg.txt_murata \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/nvram_mfg.txt_semcove:system/etc/wifi/nvram_mfg.txt_semcove \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/nvram_net.txt_murata:system/etc/wifi/nvram_net.txt_murata \
+    vendor/samsung/n7000/proprietary/system/etc/wifi/nvram_net.txt_semcove:system/etc/wifi/nvram_net.txt_semcove \
+    vendor/samsung/n7000/proprietary/system/lib/libakm.so:system/lib/libakm.so \
+    vendor/samsung/n7000/proprietary/system/lib/libril.so:system/lib/libril.so \
+    vendor/samsung/n7000/proprietary/system/lib/libsec-ril.so:system/lib/libsec-ril.so \
+    vendor/samsung/n7000/proprietary/system/lib/libsecril-client.so:system/lib/libsecril-client.so \
+    vendor/samsung/n7000/proprietary/system/lib/hw/gps.exynos4.so:system/lib/hw/gps.exynos4.so
+
